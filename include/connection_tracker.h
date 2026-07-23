409252a65071a628d50e88d53346aa3a961bd21a
MALPURE BHAVESH ASHOK <bhaveshmalpure18@gmail.com>
2026-07-23 09:14:04 +0530

    Add GlobalConnectionTable class for aggregated stats

--- include/connection_tracker.h
+++ include/connection_tracker.h
@@ -73,3 +73,52 @@ public:
     // Iteration callback for all connections
     void forEach(std::function<void(const Connection&)> callback) const;
 
+private:
+    int fp_id_;
+    size_t max_connections_;
+    
+    // Connection table
+    // Note: FiveTuple hash ensures consistent mapping, so we don't need
+    // to handle bidirectional flows specially here
+    std::unordered_map<FiveTuple, Connection, FiveTupleHash> connections_;
+    
+    // Statistics
+    size_t total_seen_ = 0;
+    size_t classified_count_ = 0;
+    size_t blocked_count_ = 0;
+    
+    // For LRU eviction if table gets full
+    void evictOldest();
+};
+
+// ============================================================================
+// Global Connection Table - Aggregates stats from all FP trackers
+// ============================================================================
+class GlobalConnectionTable {
+public:
+    GlobalConnectionTable(size_t num_fps);
+    
+    // Register an FP's tracker
+    void registerTracker(int fp_id, ConnectionTracker* tracker);
+    
+    // Get aggregated statistics
+    struct GlobalStats {
+        size_t total_active_connections;
+        size_t total_connections_seen;
+        std::unordered_map<AppType, size_t> app_distribution;
+        std::vector<std::pair<std::string, size_t>> top_domains;
+    };
+    
+    GlobalStats getGlobalStats() const;
+    
+    // Generate report
+    std::string generateReport() const;
+
+private:
+    std::vector<ConnectionTracker*> trackers_;
+    mutable std::shared_mutex mutex_;
+};
+
+} // namespace DPI
+
+#endif // CONNECTION_TRACKER_H
